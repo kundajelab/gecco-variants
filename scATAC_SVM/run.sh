080e0bbd8c9a53a1b13507ca8e314bc78949b56a
anna shcherbina <annashcherbina@gmail.com>
2020-02-21 17:36:53 -0800

    modularized SVM input preparation pipeline

---
+++ scATAC_SVM/run.sh
@@ -0,0 +1,15 @@
+task=COLL
+idr=COLL.idr.optimal_peak.narrowPeak.summits.max.signal
+echo "starting $task $idr" 
+get_svm_peak_splits.sh $task $idr
+echo "got svm peak splits" 
+get_gc_positives.sh $task
+echo "got gc content of the positive sequences" 
+get_all_negatives.sh $task $idr
+echo "got candidate negative set" 
+get_chrom_gc_region_dict.sh $task
+echo "created python pickle for candidate negatives" 
+form_svm_input_fastas.sh $task
+echo "finished creating SVM inputs" 
+
+ 
