da2adeff50e7b99207c4a6374ba970778677d34a
anna shcherbina <annashcherbina@gmail.com>
2020-05-18 16:46:20 -0700

    gecco repo sync

---
+++ make_pygenometracks/download_tracks/optimal.overlap.hammock.sh
@@ -0,0 +1,81 @@
+wget -O HCT116_H3K27ac.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/Batch1/chip_seq/h3k27ac/HCT116_H3K27ac/out/peak/macs2/overlap/optimal_set/HCT116_H3K27ac_rep1-pr.naive_overlap.filt.narrowPeak.hammock.gz
+
+wget -O 747T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/747T/cromwell-executions/chip/dc7ac9b3-a2ce-4cd0-ad91-41f7d4203493/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 589N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/589N/cromwell-executions/chip/3
+49e9020-56c3-4c71-a705-ec59efa14b28/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 704T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/704T/cromwell-executions/chip/8
+e99c8d3-9450-4189-93f9-185114dd28da/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 703T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/703T/cromwell-executions/chip/366830c7-b947-464a-9826-8aad191e5ad4/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 755T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/755T/cromwell-executions/chip/9122685c-287f-4fa4-8102-dab167a47938/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 176T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/176T/cromwell-executions/chip/c04a3190-3ac0-4e92-befd-382cff62dd83/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 175T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/175T/cromwell-executions/chip/261b059d-45c3-40f0-9d8f-c0730ff9a4ad/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 620.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/620N/cromwell-executions/chip/ebf43009-01af-4485-86e1-6cd9497f3440/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 858T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/858T/cromwell-executions/chip/600a8fdb-a87b-4b79-ac87-5b787cd9f4e9/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 389T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/389T/cromwell-executions/chip/fa8f5732-f99c-46c3-a627-f0ffb82a1e0d/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 391N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/391N/cromwell-executions/chip/12b9718f-b6c2-4d9d-98e8-ddc763be20b0/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 651T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/651T/cromwell-executions/chip/358c0ed3-058f-4138-84a9-0975a2f652cb/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 644N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/644N/cromwell-executions/chip/330cf0bc-c01e-4717-930e-2369e7d51b27/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 260N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/260N/cromwell-executions/chip/8d35dd40-e955-425d-b5e2-8e0c8fc397de/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 235N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/235N/cromwell-executions/chip/2f65852c-429c-4fe1-82e8-db49d8ba0971/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 589T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/589T/cromwell-executions/chip/793111eb-eae8-454a-8cb2-7340d336c8bb/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 175N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/175N/cromwell-executions/chip/482bf328-ce99-4899-9a3d-dea1e6d2369d/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 492.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/492N/cromwell-executions/chip/0ae9d25d-4471-448f-bae1-3ffb91943578/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 755N.overlap.filt.narrowPeak.hammock.gz  http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/755N/cromwell-executions/chip/887f4804-91b6-44fc-9fe1-d4f39b021588/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 626.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/626N/cromwell-executions/chip/1eca7e99-9ed2-49fa-af6b-517165a3773b/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 644T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/644T/cromwell-executions/chip/bb220520-c340-4159-9aca-010a24588e22/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 402T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/402T/cromwell-executions/chip/d39cb42d-fa99-4491-a14f-b511b8d92159/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 391T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/391T/cromwell-executions/chip/5a117a17-15b0-485d-9c5b-79a5bcc17bff/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 620T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/620T/cromwell-executions/chip/bd825ece-5f2c-43bb-ae26-c98603c57804/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 235T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/235T/cromwell-executions/chip/c16bca0f-ebd4-465a-b457-df9bd9d4ad72/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 4925.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/492T/cromwell-executions/chip/77c62f47-f9bb-41d2-a363-e6264235fefa/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 747N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/747N/cromwell-executions/chip/29ca9433-6d90-428d-8403-73c405e76252/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 697T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/697T/cromwell-executions/chip/ebc58421-4ee3-4b88-ad05-b57cdb33ddde/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 260T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/260T/cromwell-executions/chip/38d08f94-fd09-4930-b3be-d20cf0e0ee84/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 505T.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/505T/cromwell-executions/chip/e3ef153a-fd8e-4070-ba87-9c919c3af95b/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 402N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/402N/cromwell-executions/chip/f84a38ef-3f71-40b8-a2ba-109f5429bdd8/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 703N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/703N/cromwell-executions/chip/64c9effc-299c-466b-b37a-c3838cc06493/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 704N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/704N/cromwell-executions/chip/35fca719-de56-492a-9d5e-d6e5a7a9469c/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 505N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/505N/cromwell-executions/chip/b3c49c24-debb-4785-872a-868d5b3ed283/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 389N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/389N/cromwell-executions/chip/5c1b1e8c-1aeb-43df-9b8a-df9ff20511e7/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 651N.overlap.filt.narrowPeak.hammock.gz  http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/651N/cromwell-executions/chip/5e6b58cb-6c55-4769-ba29-13867d87354c/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 176N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/176N/cromwell-executions/chip/b5ed7be3-1cfa-4cdc-8201-954fc503e67e/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 697N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/697N/cromwell-executions/chip/2f540bf5-f3a1-41d9-84a1-87af51959aee/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
+
+wget -O 858N.overlap.filt.narrowPeak.hammock.gz http://mitra.stanford.edu/kundaje/projects/GECCO/scacheri_46_h3k27ac_chipseq/output_hg19/858N/cromwell-executions/chip/c3ecb626-c637-47c0-90fa-9462cf416d87/call-reproducibility_overlap/execution/optimal_peak.narrowPeak.hammock.gz
