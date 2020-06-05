ff80c65905f108089b9e93a8d1f3ce3a5d9d04b0
Anna Shcherbina <annashcherbina@gmail.com>
2020-06-05 13:54:17 -0700

    batching  up gkmexplain runs for genomewide gecco scores

--- genomewide_scores/explain.sh
+++ genomewide_scores/explain.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-split=$1
-cell=$2 
+fold=$1
+task=$2 
+group=$3
 prefix=/oak/stanford/groups/akundaje/projects/GECCO/genome_wide_scores
-gkmexplain  $prefix/fasta/$split.gecco.snps.in.dnase.peaks.1kb.fa $prefix/models/model.$cell.$split.model.txt $prefix/out/gkmexplain.$cell.$split.txt
+gkmexplain  $prefix/rerun/$fold.$task.fa.$group $prefix/models/model.$task.$fold.model.txt $prefix/out/gkmexplain.$task.$fold.txt.$group
 
 
