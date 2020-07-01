741885c3324232be357ef23c260b07e9a6950b30
Anna Shcherbina <annashcherbina@gmail.com>
2020-07-01 12:09:30 -0700

    organizing ldsc scripts

--- ldsc/scripts/sbatch_ldsc_singlecell.sherlock.sh
+++ ldsc/scripts/sbatch_ldsc_singlecell.sherlock.sh
@@ -1,14 +1,14 @@
 tissue_annotations=/oak/stanford/groups/akundaje/projects/GECCO/ldsc/tissue_specific_snp_annotation
 out_dir=/oak/stanford/groups/akundaje/projects/GECCO/ldsc/ldsc_score_regression
-mkdir logs 
-for chrom in `seq 1 22`
+#mkdir logs 
+for chrom in `seq 4 22`
 do
-    for peak_set in model_peaks roadmap_enhancer_annotations roadmap_peaks scENCODe #htan
+    for peak_set in roadmap_enhancer_annotations  #htan model_peaks scENCODE roadmap_peaks
     do
-	mkdir logs/$peak_set 
-	for region in `cat $tissue_annotations/$peak_set`
+	#mkdir logs/$peak_set 
+	for region in `cat $tissue_annotations/$peak_set.txt`
 	do
-	    sbatch -J $region.$chrom -o logs/$peak_set.o -e logs/$peak_set.e -p euan,owners,akundaje ldscore_singlecell.sherlock.sh $chrom $tissue_annotations/$peak_set/$region.chr$chrom.annot $out_dir/$peak_set/$region.chr$chrom
+	    sbatch -J $chrom.$region -o logs/$peak_set/$chrom.$region.o -e logs/$peak_set/$chrom.$region.e -p euan,owners,akundaje ldscore_singlecell.sherlock.sh $chrom $tissue_annotations/$peak_set/$region.chr$chrom.annot $out_dir/$peak_set/$region.chr$chrom
 	done
     done
 done
