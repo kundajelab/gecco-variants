663a164424e948eeea7232f86ac26940599e69a4
anna shcherbina <annashcherbina@gmail.com>
2020-07-01 12:59:15 -0700

    updating tissue specific hertiatiblity partition scripts to run on gecco, htan, encode sc, roadmap peaks, roadmap enhancers

--- ldsc/scripts/cell_type_spe_regression.sh
+++ ldsc/scripts/cell_type_spe_regression.sh
@@ -10,30 +10,28 @@ unset DISPLAY XAUTHORITY
 export sumstats_fn="/oak/stanford/groups/akundaje/projects/GECCO/ldsc_colorectal_cancer_gwas_summary_statistics/munged.txt.gz"
 # Load pre-munged GWAS sumstats files, in LDSC format
 # Input data and settings
-export config_dir='/srv/scratch/annashch/gecco/ldsc_htan/config_partition_heritability_merged'
-export tissue_specific_annotation_dir='/srv/scratch/annashch/gecco/ldsc_htan/tissue_specific_snp_annotation'
+export config_dir='/srv/scratch/annashch/gecco/ldsc/config_partition_heritability_merged/all.ldcts'
 export weight_dir="/mnt/data/ldscore/weights_hm3_no_hla"
 export frq_dir="/mnt/data/ldscore/1000G_frq"
 export baseline_annotation_dir='/mnt/data/ldscore/baseline'
 # Output directory
-export out_dir='/srv/scratch/annashch/gecco/ldsc_htan/partition_heritability_merged/'
+export out_dir='/srv/scratch/annashch/gecco/ldsc/partition_heritability_merged/'
 
 cell_type_SPE(){
-    mkdir -p $2
-    echo INFO - in_dir: $1
-    echo INFO - out_dir: $2
-    echo INFO - trait: $3
-    echo INFO - gwas: $4
-    echo INFO - ldcts: $5
-    if [[ -e "$2/$3.merged.results" ]]; then	
+    mkdir -p $1
+    echo INFO - out_dir: $1
+    echo INFO - trait: $2
+    echo INFO - gwas: $3
+    echo INFO - ldcts: $4
+    if [[ -e "$1/$2.merged.results" ]]; then	
 	echo "INFO - Already finished. Skipping."
 	return 0	
     fi 
     ldsc.py \
-	--h2-cts $4 \
+	--h2-cts $3 \
 	--ref-ld-chr $baseline_annotation_dir/baseline. \
-	--out $2/$3.merged \
-	--ref-ld-chr-cts $5 \
+	--out $1/$2.merged \
+	--ref-ld-chr-cts $4 \
 	--w-ld-chr $weight_dir/weights.
 }
 
